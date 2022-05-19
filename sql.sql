1ada3866b03f80cb61a498d48e35bebfa7bc9f73
owgrunt <artem.samarsky@gmail.com>
2022-05-19 16:58:23 +0000

    Thu, May 19, 2022, 4:58 PM +00:00

--- sql.sql
+++ sql.sql
@@ -5,15 +5,14 @@
 
 SELECT
    team_performances.team_id,
-   speaker_one_id,
-   rating
+   speaker_one.rating + speaker_two.rating
 FROM
    team_performances
 INNER JOIN teams ON
     team_performances.team_id = teams.id
-INNER JOIN speakers ON
-    teams.speaker_one_id = speakers.id
-INNER JOIN speakers ON
-    teams.speaker_two_id = speakers.id
+INNER JOIN speakers AS speaker_one ON
+    teams.speaker_one_id = speaker_one.id
+INNER JOIN speakers AS speaker_two ON
+    teams.speaker_two_id = speaker_two.id
 WHERE
     team_performances.debate_id = 1;
