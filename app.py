e5011911a6034fbef1c9859dd1c8b3bc5f8aa110
owgrunt <artem.samarsky@gmail.com>
2022-05-20 15:17:02 +0000

    Fri, May 20, 2022, 3:17 PM +00:00

--- app.py
+++ app.py
@@ -572,7 +572,7 @@ def import_rounds():
             results = lookup_link(debate["url"] + "/ballots")
             # results = lookup_link(debate["url"] + "/ballots")[0]["result"]["sheets"][0]["teams"]
             # Get to the team result
-            if not results == [] and not results["detail"]:
+            if not results == [] and not "detail" results:
                 results = results[0]["result"]["sheets"][0]["teams"]
                 for result in results:
                     # Prepare data for import
