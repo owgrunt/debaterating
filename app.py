5ef5f3db73d3eb5aad9cf939ccd2cedc0783f90b
owgrunt <artem.samarsky@gmail.com>
2022-05-20 12:51:51 +0000

    Fri, May 20, 2022, 12:51 PM +00:00

--- app.py
+++ app.py
@@ -751,7 +751,7 @@ def calculate_elo():
                         victors_expected_score = 1 / ( 1 + pow(10, (loser_rating - victor_rating) / 400))
                         # Calculate how much the rating will be adjusted
                         rating_adjustment = k_factor*(1-victors_expected_score)
-                        rating_adjustment = round(rating_adjustment)
+                        # rating_adjustment = round(rating_adjustment)
                         # Adjust the ratings
                         k = 0
                         for update in updated_ratings:
