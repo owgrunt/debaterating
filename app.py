41ca13d399f980192f2d1093e76e2ecd6243160b
owgrunt <artem.samarsky@gmail.com>
2022-05-20 15:33:45 +0000

    Fri, May 20, 2022, 3:33 PM +00:00

--- app.py
+++ app.py
@@ -546,7 +546,7 @@ def import_rounds():
                     for panellist in debate["adjudicators"]["panellists"]:
                         adjudicator["internal_id"] = panellist.replace(f"https://{domain}/api/v1/tournaments/{slug}/adjudicators/", "")
                         adjudicator_internal_id = adjudicator["internal_id"]
-                        adjudicator["speaker_id"] = db.execute(f"SELECT id FROM tournament_participants WHERE speaker_internal_id = {adjudicator_internal_id} AND tournament_id = {tournament_id}")[0]["id"]
+                        adjudicator["speaker_id"] = db.execute(f"SELECT speaker_id FROM tournament_participants WHERE speaker_internal_id = {adjudicator_internal_id} AND tournament_id = {tournament_id}")[0]["speaker_id"]
                         adjudicator["role"] = "panellist"
 
                         # Import adjudication instance into the db
