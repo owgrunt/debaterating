ebe33823219fafb8b5a41ee5dc10954c9cf11ab8
owgrunt <artem.samarsky@gmail.com>
2022-05-21 14:58:50 +0000

    Sat, May 21, 2022, 2:58 PM +00:00

--- app.py
+++ app.py
@@ -916,7 +916,16 @@ def speaker_profile():
         points_by_side = points_by_side + [new_value]
 
     # Prepare data to show a pie chart of your team rankings
-    
+    team_rankings = [0, 0, 0, 0]
+    for speech in speeches:
+        if speech["team_score"] == 3:
+            team_rankings[0] = team_rankings[0] + 1
+        elif speech["team_score"] == 2:
+            team_rankings[0] = team_rankings[1] + 1
+        elif speech["team_score"] == 1:
+            team_rankings[0] = team_rankings[2] + 1
+        elif speech["team_score"] == 0:
+            team_rankings[0] = team_rankings[3] + 1
 
     # Prepare data to show your points depending on room strength
     points_by_room_strength = []
@@ -926,7 +935,7 @@ def speaker_profile():
         entry["x"] = speech["average_rating"]
         points_by_room_strength = points_by_room_strength + [entry]
 
-    return render_template("0-speaker.html", speaker=speaker, speeches=speeches, count=count, speaks_by_position=speaks_by_position, points_by_side=points_by_side, points_by_room_strength=points_by_room_strength)
+    return render_template("0-speaker.html", speaker=speaker, speeches=speeches, count=count, speaks_by_position=speaks_by_position, points_by_side=points_by_side, points_by_room_strength=points_by_room_strength, team_rankings=team_rankings)
 
 
 @app.route("/register", methods=["GET", "POST"])
