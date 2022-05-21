0aa10e2e2794aa58d7d020619153013cc2df53f6
owgrunt <artem.samarsky@gmail.com>
2022-05-21 10:18:33 +0000

    Sat, May 21, 2022, 10:18 AM +00:00

--- sql_get_speeches.sql
+++ sql_get_speeches.sql
@@ -1,33 +1,27 @@
 SELECT
---    team_performances.team_id,
---    team_performances.score,
---    team_performances.debate_id,
---    teams.swing,
---    teams.speaker_one_id AS speaker_one,
---    teams.speaker_two_id AS speaker_two,
---    speaker_one.rating AS speaker_one_rating,
---    speaker_two.rating AS speaker_two_rating
     speeches.id,
     speeches.tournament_id,
     speeches.debate_id,
     speeches.score,
     speeches.rating_change,
     speeches.position,
+    team_performances.score AS team_score
 FROM
    speeches
-INNER JOIN team_performances ON
-    team_performances.debate_id = teams.id
-INNER JOIN speakers AS speaker_one ON
-    teams.speaker_one_id = speaker_one.id
-INNER JOIN speakers AS speaker_two ON
-    teams.speaker_two_id = speaker_two.id
-INNER JOIN debates ON
-    team_performances.debate_id = debates.id
+INNER JOIN team_performances
+    ON speeches.debate_id = team_performances.debate_id
+    AND team_performances.team_id IN
+        (
+            SELECT
+                id
+            FROM
+                teams
+            WHERE
+                speeches.speaker_id = teams.speaker_one_id
+            OR
+                speeches.speaker_id = teams.speaker_two_id
+        )
 WHERE
-    team_performances.tournament_id = xxxxxx
--- AND
---     team_performances.debate_id = 8
-AND
-    debates.round_id = yyyyyy
+    speeches.speaker_id = 66
 ORDER BY
-    team_performances.debate_id ASC;
+    speeches.id ASC;
