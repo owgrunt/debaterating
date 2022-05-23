bf6b3f9c60aa9e22587f1ef3bf8e4aba0648d752
owgrunt <artem.samarsky@gmail.com>
2022-05-23 13:41:48 +0000

    Mon, May 23, 2022, 1:41 PM +00:00

--- sql.sql
+++ sql.sql
@@ -1,5 +1,9 @@
-INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Артем", "Самарский", "Сергеевич");
-INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Свинг", "Свингов", "Свингович");
+-- INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Артем", "Самарский", "Сергеевич");
+-- INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Свинг", "Свингов", "Свингович");
 -- ALTER TABLE speakers ADD internal_id INTEGER NOT NULL DEFAULT 0;
 -- DROP TABLE team_performances;
 -- DELETE FROM speakers;
+
+-- SELECT speaker_id, avg(score)
+-- FROM speeches
+-- GROUP BY speaker_id;
