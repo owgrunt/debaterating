54ed02a16a00e2f4b3c8226f38c6f10d94c6adea
owgrunt <artem.samarsky@gmail.com>
2022-05-23 13:59:11 +0000

    Mon, May 23, 2022, 1:59 PM +00:00

--- sql.sql
+++ sql.sql
@@ -7,4 +7,11 @@
 SELECT speaker_id, avg(score) AS average_score
 FROM speeches
 GROUP BY speaker_id
-ORDER BY avg(score) DESC;
+HAVING average_score IN
+    (
+        SELECT avg(score) AS average_score
+        FROM speeches
+        GROUP BY speaker_id
+        ORDER BY average_score DESC
+        LIMIT 1
+    );
