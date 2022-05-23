ebf5e34c023d0a7a89ec1e9e2fe7f74465e1b609
owgrunt <artem.samarsky@gmail.com>
2022-05-23 14:28:44 +0000

    Mon, May 23, 2022, 2:28 PM +00:00

--- sql_get_best_speaker.sql
+++ sql_get_best_speaker.sql
@@ -6,6 +6,7 @@ HAVING average_score IN
     (
         SELECT avg(score) AS average_score
         FROM speeches
+        WHERE tournament_id = xxxxxx
         GROUP BY speaker_id
         ORDER BY average_score DESC
         LIMIT 1
--- sql_get_best_speaker_in_category.sql
+++ sql_get_best_speaker_in_category.sql
@@ -1,12 +1,21 @@
-SELECT speaker_id, avg(score) AS average_score
+SELECT *
+    -- speeches.speaker_id, avg(score) AS average_score
 FROM speeches
-WHERE tournament_id = 1
-GROUP BY speaker_id
-HAVING average_score IN
-    (
-        SELECT avg(score) AS average_score
-        FROM speeches
-        GROUP BY speaker_id
-        ORDER BY average_score DESC
-        LIMIT 1
-    );
+INNER JOIN
+    speakers_in_categories ON speeches.speaker_id = speakers_in_categories.speaker_id
+WHERE
+    speeches.tournament_id = 1
+AND
+    speakers_in_categories.category_id = 1
+GROUP BY speeches.speaker_id
+-- HAVING average_score IN
+--     (
+--         SELECT avg(score) AS average_score
+--         FROM speeches
+        -- WHERE
+        --     speeches.tournament_id = 1
+--         GROUP BY speaker_id
+--         ORDER BY average_score DESC
+--         LIMIT 1
+--     )
+;
