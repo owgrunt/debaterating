430abbb728912684ff6eefc1eeb635dd94e48600
owgrunt <artem.samarsky@gmail.com>
2022-05-25 19:57:50 +0000

    Wed, May 25, 2022, 7:57 PM +00:00

--- sql_get_team_tab.sql
+++ sql_get_team_tab.sql
@@ -9,12 +9,16 @@ FROM
    team_performances tp
 INNER JOIN teams t ON
     tp.team_id = t.id
+INNER JOIN rounds r ON
+    tp.round_id = r.id
 INNER JOIN speakers AS s1 ON
     t.speaker_one_id = s1.id
 INNER JOIN speakers AS s2 ON
     t.speaker_two_id = s2.id
 WHERE
     tp.tournament_id = 1
+AND
+    r.stage != "E"
 GROUP BY
     tp.team_id
 ORDER BY
