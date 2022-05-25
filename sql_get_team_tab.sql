b7be7c7ef2f4dd9e099f357d3b45defee7e10219
owgrunt <artem.samarsky@gmail.com>
2022-05-25 19:48:20 +0000

    Wed, May 25, 2022, 7:48 PM +00:00

--- sql_get_team_tab.sql
+++ sql_get_team_tab.sql
@@ -1,6 +1,6 @@
 SELECT
    team_performances.team_id,
-   team_performances.score,
+   sum(tp.score) as team_score,
    team_performances.debate_id,
    teams.swing,
    teams.speaker_one_id AS speaker_one,
@@ -8,20 +8,16 @@ SELECT
    speaker_one.rating AS speaker_one_rating,
    speaker_two.rating AS speaker_two_rating
 FROM
-   team_performances
-INNER JOIN teams ON
-    team_performances.team_id = teams.id
-INNER JOIN speakers AS speaker_one ON
-    teams.speaker_one_id = speaker_one.id
-INNER JOIN speakers AS speaker_two ON
-    teams.speaker_two_id = speaker_two.id
+   team_performances tp
+INNER JOIN teams t ON
+    tp.team_id = t.id
+INNER JOIN speakers AS s1 ON
+    t.speaker_one_id = s1.id
+INNER JOIN speakers AS s2 ON
+    t.speaker_two_id = s2.id
 INNER JOIN debates ON
     team_performances.debate_id = debates.id
 WHERE
-    team_performances.tournament_id = xxxxxx
--- AND
---     team_performances.debate_id = 8
-AND
-    debates.round_id = yyyyyy
+    team_performances.tournament_id = 1
 ORDER BY
-    team_performances.debate_id ASC;
+    team_score DESC;
