910eec826716aa48faac6372abbb0e2cc0f02b28
owgrunt <artem.samarsky@gmail.com>
2022-05-26 13:59:16 +0000

    Thu, May 26, 2022, 1:59 PM +00:00

--- sql.sql
+++ sql.sql
@@ -1,7 +1,17 @@
 INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Артем", "Самарский", "Сергеевич");
 INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Ирина", "Сергеева", "Юрьевна");
 INSERT INTO speakers (first_name, last_name, middle_name) VALUES ("Свинг", "Свингов", "Свингович");
-INSERT INTO speakers (first_name, last_name) VALUES ("Архипов", "Святослав");
+INSERT INTO speakers (first_name, last_name) VALUES ("Святослав", "Архипов");
+INSERT INTO speakers (first_name, last_name) VALUES ("Мария", "Лазарук");
+INSERT INTO speakers (first_name, last_name) VALUES ("Риза", "Абасов");
+INSERT INTO speakers (first_name, last_name) VALUES ("Рустам", "Байголопов");
+INSERT INTO speakers (first_name, last_name) VALUES ("Михаил", "Волобуев");
+INSERT INTO speakers (first_name, last_name) VALUES ("Адиля", "Ихсанова");
+INSERT INTO speakers (first_name, last_name) VALUES ("Мария", "Елисоветская");
+INSERT INTO speakers (first_name, last_name) VALUES ("Алексей", "Никитин");
+INSERT INTO speakers (first_name, last_name) VALUES ("Арина", "Халиуллина");
+INSERT INTO speakers (first_name, last_name) VALUES ("Родион", "Белов");
+INSERT INTO speakers (first_name, last_name) VALUES ("Айзат", "Гадиев");
 -- ALTER TABLE speakers ADD internal_id INTEGER NOT NULL DEFAULT 0;
 -- DROP TABLE team_performances;
 -- DELETE FROM speakers;
