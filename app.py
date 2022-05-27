8108c474bd418f9a007d30fb1f74545d209f188c
owgrunt <artem.samarsky@gmail.com>
2022-05-27 22:18:59 +0000

    Fri, May 27, 2022, 10:18 PM +00:00

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///debaterating.db")
 # test
+
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://")
