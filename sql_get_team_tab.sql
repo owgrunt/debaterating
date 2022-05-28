b9f350f961b7dc751bcb9d34aaf22538cde13a28
owgrunt <artem.samarsky@gmail.com>
2022-05-28 14:15:48 +0000

    Sat, May 28, 2022, 2:15 PM +00:00

--- sql_get_team_tab.sql
+++ sql_get_team_tab.sql
@@ -1,21 +1,15 @@
 SELECT
-   tp.team_id,
-   sum(tp.score) as team_score,
---    (
---         SELECT sum(rating_change)
---         FROM speeches sp
---         WHERE t.speaker_one_id = sp.speaker_id
---         AND tp.tournament_id = xxxxxx
---    ) AS elo,
-    sum(sp.rating_change),
+    tp.team_id,
+    sum(tp.score) AS team_score,
+    sum(sp.rating_change) AS rating_change,
     s1.first_name AS s1_first_name,
-   s2.first_name AS s2_first_name,
-   s1.last_name AS s1_last_name,
-   s2.last_name AS s2_last_name,
-   s1.id AS s1_id,
-   s2.id AS s2_id
+    s2.first_name AS s2_first_name,
+    s1.last_name AS s1_last_name,
+    s2.last_name AS s2_last_name,
+    s1.id AS s1_id,
+    s2.id AS s2_id
 FROM
-   team_performances tp
+    team_performances tp
 LEFT JOIN teams t ON
     tp.team_id = t.id
 LEFT JOIN debates d ON
