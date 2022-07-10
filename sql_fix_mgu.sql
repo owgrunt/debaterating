0baba2fe6c9bfe348fc66c0eec3a581a06acb4b7
owgrunt <artem.samarsky@gmail.com>
2022-07-10 16:22:38 +0000

    Sun, Jul 10, 2022, 4:22 PM +00:00

--- sql_fix_mgu.sql
+++ sql_fix_mgu.sql
@@ -2,6 +2,12 @@
 -- UPDATE tournament_participants SET role = "ca" WHERE internal_name = "Артем Посвежинский" AND tournament_id = 1;
 -- UPDATE tournament_participants SET role = "ca" WHERE internal_name = "Роман Игнатенко" AND tournament_id = 1;
 -- UPDATE tournament_participants SET role = "ca" WHERE internal_name = "Глеб Игнатьев" AND tournament_id = 1;
-INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 91, "adjudicator", "лучший судья");
-INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 92, "adjudicator", "лучший судья");
-INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 90, "adjudicator", "лучший судья");
+-- INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 91, "adjudicator", "лучший судья");
+-- INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 92, "adjudicator", "лучший судья");
+-- INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 90, "adjudicator", "лучший судья");
+
+update tournament_participants set role = 'adjudicator' where tournament_id = 1 and role = 'ca';
+update tournament_participants set role = 'ca' where tournament_id = 1 and speaker_id = 90 or speaker_id = 7 or speaker_id = 91;
+INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 92, 'adjudicator', 'лучший судья');
+INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 93, 'adjudicator', 'лучший судья');
+INSERT INTO achievements (tournament_id, speaker_id, type, name) VALUES (1, 94, 'adjudicator', 'лучший судья');
