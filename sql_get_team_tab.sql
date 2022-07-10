ad9a9b0c7194e8ef9e521d9daf27099aa33ddec3
owgrunt <artem.samarsky@gmail.com>
2022-07-10 16:44:01 +0000

    Sun, Jul 10, 2022, 4:44 PM +00:00

--- sql_get_team_tab.sql
+++ sql_get_team_tab.sql
@@ -1,7 +1,8 @@
 SELECT
     tp.team_id,
     sum(tp.score) AS team_score,
-    sp.rating_change AS rating_change,
+    sp1.rating_change AS rating_change_one,
+    sp2.rating_change AS rating_change_two,
     s1.first_name AS s1_first_name,
     s2.first_name AS s2_first_name,
     s1.last_name AS s1_last_name,
@@ -27,13 +28,22 @@ INNER JOIN
         WHERE tournament_id = xxxxxx
         GROUP BY speaker_id
     )
-    sp ON
-    t.speaker_one_id = sp.speaker_id
+    sp1 ON
+    t.speaker_one_id = sp1.speaker_id
+INNER JOIN
+    (
+        SELECT sum(rating_change) AS rating_change, speaker_id
+        FROM speeches
+        WHERE tournament_id = xxxxxx
+        GROUP BY speaker_id
+    )
+    sp2 ON
+    t.speaker_two_id = sp2.speaker_id
 WHERE
     tp.tournament_id = xxxxxx
 AND
     r.stage != 'E'
 GROUP BY
-    tp.team_id, t.speaker_one_id, s1.first_name, s2.first_name, s1.last_name, s2.last_name, s1.id, s2.id, sp.rating_change
+    tp.team_id, t.speaker_one_id, s1.first_name, s2.first_name, s1.last_name, s2.last_name, s1.id, s2.id, sp1.rating_change, sp2.rating_change
 ORDER BY
     team_score DESC;
