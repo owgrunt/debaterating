7f975b7d1b255128204c29f9a92fbbdfeccf40d6
owgrunt <artem.samarsky@gmail.com>
2022-07-10 15:55:30 +0000

    Sun, Jul 10, 2022, 3:55 PM +00:00

---
+++ drop_postgres.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS
+	speakers,
+	societies,
+	tournaments,
+	teams,
+	rounds,
+	debates,
+	speeches,
+	team_performances,
+	tournament_participants,
+	adjudications,
+	break_categories,
+	achievements,
+	speaker_categories,
+	speakers_in_categories
+CASCADE;
