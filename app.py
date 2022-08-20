2f5c59b599cc1b9fc6e9f0112c0fb58cd2276bc1
owgrunt <artem.samarsky@gmail.com>
2022-08-20 14:50:41 +0000

    Sat, Aug 20, 2022, 2:50 PM +00:00

--- app.py
+++ app.py
@@ -1298,7 +1298,7 @@ def calculate_speaker_scores():
         id = speaker["id"]
         db.execute(f"UPDATE speakers SET ranking_by_speaks = {ranking_by_speaks}, ranking_by_rating = {ranking_by_rating} WHERE id = {id}")
 
-    speakers = db.execute(f"SELECT * FROM speakers RIGHT JOIN tournament_participants)
+    speakers = db.execute(f"SELECT * FROM speakers RIGHT JOIN tournament_participants ON )
 
     return render_template("import/speaker-scores.html", speakers=speakers)
 
