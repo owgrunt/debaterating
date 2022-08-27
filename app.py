cd919b45c3f34cb876f770c2480e9c8a3c0007fc
owgrunt <artem.samarsky@gmail.com>
2022-08-27 13:35:50 +0000

    Sat, Aug 27, 2022, 1:35 PM +00:00

--- app.py
+++ app.py
@@ -1466,7 +1466,7 @@ def edit_speaker():
         else:
             candidates = db.execute(f"SELECT * FROM speakers WHERE last_name = ? AND first_name = ? AND middle_name = ?",
                                     speaker["last_name"], speaker["first_name"], speaker["middle_name"])
-        if len(candidates) > 0 or :
+        if len(candidates) > 0:
             return apology("speaker already exists", 400)
         else:
             db_name = "speakers"
