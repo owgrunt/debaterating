b823d55ab64577ce0da907425980137ba09858a0
owgrunt <artem.samarsky@gmail.com>
2022-09-15 17:43:17 +0000

    Thu, Sep 15, 2022, 5:43 PM +00:00

--- app.py
+++ app.py
@@ -33,17 +33,18 @@ if uri.startswith("postgres://"):
 db = SQL(uri)
 
 
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+#     """Ensure responses aren't cached"""
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
 
 
